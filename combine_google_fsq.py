0f89b4d24865eb1fe7fccef997120c90b7b2b12f
bill <milias.vasilis@gmail.com>
2018-06-14 19:35:52 +0200

    added matching functions

--- combine_google_fsq.py
+++ combine_google_fsq.py
@@ -1,13 +1,13 @@
 import sys
 sys.path.append("..")
 import postgis_functions
-
 from difflib import SequenceMatcher
 from pyxdameraulevenshtein import  normalized_damerau_levenshtein_distance
 import fuzzy
 import Levenshtein
 from nltk.corpus import wordnet
 from transliterate import translit
+import pois_storing_functions
 
 
 def longest_substring(str1, str2):
@@ -45,21 +45,27 @@ def get_levenshtein_phonetic_similarity(osm_name, source_name):
     try:
         dmeta_osm = dmeta(osm_name)[0]#.decode("utf-8")
         dmeta_source = dmeta(source_name)[0]#.decode("utf-8")
-    except:
+    except Exception as err:
         return None
     return Levenshtein.ratio(dmeta_osm, dmeta_source)
 
 
 def get_str_similarity(source, target):
+    long_sub = longest_substring(source, target)
+    print(long_sub)
+    # "long_substring": longest_substring(source, target),
     similarities = {
-        "long_substring": longest_substring(source, target),
         "ro_similarity": get_ro_similarity(source, target),
         "dleven_similarity": 1 - normalized_damerau_levenshtein_distance(source, target),
         "leven_similarity": Levenshtein.ratio(source, target),
         "phonetic_similarity": get_levenshtein_phonetic_similarity(source, target),
     }
-    similarities["len_long_substring"] = len(similarities["long_substring"])
-    return similarities
+    if (len(long_sub)):
+        similarities["len_long_substring"] = len(long_sub)/len(source)
+    else:
+        similarities["len_long_substring"] = 0
+    res = {k: v for k, v in similarities.items() if v is not None}
+    return res
 
 
 def get_gf_similarity_score(google_info, fsq_info):
@@ -67,35 +73,146 @@ def get_gf_similarity_score(google_info, fsq_info):
     # (and not the girlfriend similarity score! :p )
     print("ok")
 
+
+def match_by_phone(fpoint, gpoint, thres):
+    try:
+        if Levenshtein.ratio(fpoint["phone"], gpoint["phone"])>=thres:
+            return True
+    except:
+        pass
+    return False
+
+
+def match_by_addr(fpoint, gpoint):
+    try:
+        if fpoint["street"] and fpoint["streetnum"] and \
+                fpoint["street"] == gpoint["streetlng"] and fpoint["streetnum"] == gpoint["streetnumlng"]:
+            return True
+    except:
+        pass
+    return False
+
+
+def match_by_name(fpoint, gpoint, thres):
+    if Levenshtein.ratio(fpoint["name"], gpoint["name"])>=thres:
+        return True
+    return False
+
+
+def match_by_street(fpoint, gpoint, thres):
+    try:
+    #if "street" in fpoint and "streetlng" in gpoint and fpoint["street"] and gpoint["streetlng"]:
+        if Levenshtein.ratio(fpoint["street"], gpoint["streetlng"])>=thres:
+            return True
+    except:
+        pass
+    return False
+
+
+def match_by_website(fpoint, gpoint, thres):
+    try:
+    #if "website" in fpoint and "website" in gpoint and fpoint["website"] and gpoint["website"]:
+        if Levenshtein.ratio(fpoint["website"], gpoint["website"])>=thres:
+            return True
+    except:
+        pass
+    return False
+
+
+def print_matched(fpoint, gpoint, attr):
+    print("~~~~ MATCH ~~~~ ", attr)
+    print("~FSQ: ", fpoint)
+    print("~GOO: ", gpoint)
+
+
+def add_matched_to_db(session, FTable, GTable, MTable, fpoint, gpoint, reason):
+    match = {"id": fpoint["id"] + "_" + gpoint["id"], "fsqid": fpoint["id"],
+             "googleid": gpoint["id"], "reason": reason}
+    try:
+        session.add(
+            FTable(**fpoint), GTable(**gpoint), MTable(**match))
+        session.commit()
+        print("~~ MATCH INSERTED!")
+        print(fpoint)
+        print(gpoint)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    except Exception as err:
+        print("~~ MATCH NOT INSERTED")
+        print(err)
+
+
 if __name__ == "__main__":
-
-    cg = postgis_functions.connect_to_db()
-    cf = postgis_functions.connect_to_db()
-
-    # get long/lat from google pois
-    #gpoints = postgis_functions.get_row_and_ll_of_pois(cg, "google_ams_center_40")
-    fpoints = postgis_functions.get_row_and_ll_of_pois(cf, "fsq_ams_center_40")
-
     # choose radius
-    rad = 20
+    #for rad in [275]:
+    fpoints = postgis_functions.get_pois_for_matching("fsq_ams_places", 0)
+    # Create matched tables
+    session, FTable = pois_storing_functions.setup_db("fsq_ams_matched_table", "", "fsq_matched")
+    session.close()
+    session, GTable = pois_storing_functions.setup_db("google_ams_matched_table", "", "google_matched")
+    session.close()
+    session, MTable = pois_storing_functions.setup_db("google_fsq_ams_matching_table", "", "matching_table")
+    rad = 300
     ###############
     # FOR EACH POI#
     ###############
-    for i, lat, lng in fpoints:
-        #fsq_closest_points = postgis_functions.get_matching_attr_from_pois_within_radius(cf, "fsq_ams_center_40",
-         #                                                                (lat, lng), rad)
-        google_closest_points = postgis_functions.get_matching_attr_from_pois_within_radius\
-            (cg, "google_ams_center_40", (lat, lng), rad)
-        #google_row = postgis_functions.get_row_from_index(cg, "google_ams_center_40", i)
-        fsq_row = postgis_functions.get_row_from_index(cf, "fsq_ams_center_40", i)
+    countname=0
+    countweb=0
+    countphone=0
+    countaddr=0
+    point = 0
+    countwebphone = 0
+    countnamedist = 0
 
-        #print("~ GOOGLE: ", google_row[0][1:5], google_row[0][8], google_row[0][10])
-        print("~ FSQ: ", fsq_row[0][1:5], fsq_row[0][8:10])
-
-        for google_row in google_closest_points:
-            print("~ GOOGLE :", google_row[1:5], google_row[8:10])
+    for fpoint in fpoints:
+        if point>253:
+            break
         print("#################################################################################")
+        print("POINT: ", point)
+        print("COUNT (addr): ", countaddr)
+        print("COUNT (Phone): ", countphone)
+        print("COUNT (Name): ", countname)
+        print("COUNT (web): ", countweb)
+        print("COUNT (webphone): ", countwebphone)
+        print("COUNT (namedist): ", countnamedist)
+        print("TOTAL: ", countaddr +countphone+countweb+countname + countwebphone + countnamedist)
+        print("@@@@@@@@@@@@@@@@@@@@@")
+        point+=1
+        # Gather places within radius
+        google_closest_points = postgis_functions.get_matching_attr_from_pois_within_radius\
+             ("google_ams_whole_clipped_40", fpoint["lat"], fpoint["lng"], rad)
 
-            # for row in fsq_closest_points_c:
-        #     print(row)
+        for gpoint in google_closest_points:
+            # match by addr total and name 0.3
+            if match_by_addr(fpoint, gpoint) and match_by_name(fpoint, gpoint, 0.3):
+                #print_matched(fpoint, gpoint, "addr_name")
+                add_matched_to_db(session,FTable, GTable, MTable, fpoint, gpoint, "addr_name")
+                countaddr+=1
+                break
+            # Match by phone  (if it exists) (total match!)
+            elif match_by_phone(fpoint, gpoint, 1):
+                #print_matched(fpoint, gpoint, "phone")
+                countphone+=1
+                break
+            # match by name and street
+            elif match_by_name(fpoint, gpoint, 0.7) and match_by_street(fpoint, gpoint, 0.8):
+                #print_matched(fpoint, gpoint, "name/street")
+                countname+=1
+                break
+            # match by website and street
+            elif match_by_website(fpoint, gpoint, 0.8) and match_by_street(fpoint, gpoint, 0.8):
+                #print_matched(fpoint, gpoint, "web/street")
+                countweb+=1
+                break
+            elif match_by_website(fpoint, gpoint, 0.8) and match_by_name(fpoint, gpoint, 0.6):
+                #print_matched(fpoint, gpoint, "web/name")
+                countwebphone+=1
+                break
+            elif match_by_name(fpoint, gpoint, 0.8) and postgis_functions.get_distance(fpoint, gpoint)<=20:
+                # print(postgis_functions.get_distance(fpoint, gpoint))
+                #print_matched(fpoint, gpoint, "dist_name")
+                countnamedist+=1
+                break
+    print("RAD = ", rad)
+    print("Total Points Matched = ", countname + countweb + countphone + countaddr + countwebphone + countnamedist)
+
 
--- postgis_functions.py
+++ postgis_functions.py
@@ -1,5 +1,14 @@
 import sqlite3
 import psycopg2
+import psycopg2.extras
+
+
+def get_distance(fpoint, gpoint):
+    conn = psycopg2.connect(database="pois", user="postgres", password="postgres")
+    c = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    c.execute("SELECT ST_DISTANCE_SPHERE(ST_MAKEPOINT({flat},{flng}), ST_MAKEPOINT({glat},{glng}))"\
+              .format(flat=fpoint["lat"], flng=fpoint["lng"], glat=gpoint["lat"], glng=gpoint["lng"]))
+    return c.fetchall()[0]["st_distance_sphere"]
 
 
 def get_points_from_db(tab, last_searched):
@@ -13,14 +22,14 @@ def get_points_from_db(tab, last_searched):
               "WHERE rn>={last_row}".format(table=tab, last_row=last_searched))
     return c
 
-
-def get_attr_for_matching_from_index(c, table):
-    c.execute("SELECT ST_x(geom), ST_y(geom) FROM {the_table}".format(the_table=table))
-    return c.fetchall()
+#
+# def get_attr_for_matching_from_index(c, table):
+#     c.execute("SELECT ST_x(geom), ST_y(geom) FROM {the_table}".format(the_table=table))
+#     return c.fetchall()
 
 
 def get_ll_from_geom(c, table):
-    c.execute("SELECT ST_x(geom), ST_y(geom) FROM {the_table}".format(the_table=table))
+    c.execute("SELECT lat, lng FROM {the_table}".format(the_table=table))
     return c
 
 
@@ -34,12 +43,42 @@ def get_row_from_index(c, table, index):
     return c.fetchall()
 
 
-def get_matching_attr_from_pois_within_radius(c, table, ll, radius):
-    c.execute("SELECT id, originalpointindex, name, type1, type2, type3, type4, "
+def get_pois_for_matching(tab, last_searched):
+    conn = psycopg2.connect(database="pois", user="postgres", password="postgres")
+    c = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    c.execute("SELECT rn, id, originalpointindex, name, type1, type2, type3, type4, "
+              "website, street, streetnum, phone, lat, lng "
+              "FROM "
+              "(SELECT row_number() OVER (ORDER BY originalpointindex NULLS LAST) AS rn,  "
+              "id, originalpointindex, lat, lng, name, type1, type2, type3, type4, "
+              "website, streetnum, street, phone  "
+              " FROM {table} ) AS rowselection "
+              "WHERE rn>={last_row}".format(table=tab, last_row=last_searched))
+    return c
+
+
+def get_matching_attr_from_pois_within_radius(table, lng, lat, rad):
+    conn = psycopg2.connect(database="pois", user="postgres", password="postgres")
+    c = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    c.execute("SELECT id, name, type1, type2, type3, type4, lat, lng, "
               "website, streetlng, streetnumlng, phone "
               "FROM {geotable} "
-              "WHERE ST_DWithin(geom::geography, ST_MakePoint{ll}, {rad})"
-              .format(geotable=table, ll=ll, rad=radius))
+              "WHERE ST_DWithin(geom::geography, ST_MakePoint({lat},{lng})::geography,{rad})"
+              "ORDER BY originalpointindex"
+              .format(geotable=table, lng=lng, lat=lat, rad=rad))
+    return c
+
+
+def get_pois_from_name(table, poi_name):
+    if "'" in poi_name:
+        poi_name = poi_name.replace("'", "''")
+    conn = psycopg2.connect(database="pois", user="postgres", password="postgres")
+    c = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    c.execute("SELECT id, name, type1, type2, type3, type4, "
+              "website, streetlng, streetnumlng, phone "
+              "FROM {geotable} "
+              "WHERE name ILIKE '%{poi_name}%'"
+              .format(geotable=table, poi_name=poi_name))
     return c
 
 
