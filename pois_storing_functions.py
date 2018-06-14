7325111a625be3f79a2671540d9c5ff11f25ebbd
bill <milias.vasilis@gmail.com>
2018-06-14 19:36:26 +0200

    maching attrs added

--- pois_storing_functions.py
+++ pois_storing_functions.py
@@ -112,6 +112,54 @@ def create_twitter_pois_table(engine, table_name, metadata):
         metadata.create_all()
 
 
+def create_matching_table(engine, table_name, metadata):
+    # if table does not exist
+    if not engine.dialect.has_table(engine, table_name):
+        Table(table_name, metadata,
+              Column("id", Numeric, primary_key=True, nullable=False),
+              Column("fsqid", Numeric),
+              Column("googleid", Numeric),
+              Column("reason", String)
+              )
+        metadata.create_all()
+
+
+
+def create_scene_features_table(engine, table_name, metadata):
+    # if table does not exist
+    if not engine.dialect.has_table(engine, table_name):
+        Table(table_name, metadata,
+              Column("id", String, primary_key=True, nullable=False),
+              Column("placesid", String),
+              Column("panosid", String),
+              Column("head", String),
+              Column("typeofenvironment", String),
+              Column("scene1", String),
+              Column("scene1prob", Float),
+              Column("scene2", String),
+              Column("scene2prob", Float),
+              Column("scene3", String),
+              Column("scene3prob", Float),
+              Column("scene4", String),
+              Column("scene4prob", Float),
+              Column("sceneattr1", String),
+              Column("sceneattr2", String),
+              Column("sceneattr3", String),
+              Column("sceneattr4", String),
+              Column("sceneattr5", String),
+              Column("sceneattr6", String),
+              Column("sceneattr7", String),
+              Column("sceneattr8", String),
+              Column("sceneattr9", String),
+              Column("year", Numeric),
+              Column("month", Numeric),
+              Column("path", String),
+              Column("lat", Numeric),
+              Column("lng", Numeric),
+              Column("geom", Geometry('Point')))
+        metadata.create_all()
+
+
 def create_count_per_poi_table(engine, table_name, metadata):
     # if table does not exist
     if not engine.dialect.has_table(engine, table_name):
@@ -138,36 +186,56 @@ def setup_db(pois_table_name, count_table_name, source):
     # change it in GTable as well!
     if source == "google":
         create_google_pois_table(db, pois_table_name, metadata)
+    elif source == "matching_table":
+        create_matching_table(db, pois_table_name, metadata)
     elif source == "FSQ":
         create_fsq_pois_table(db, pois_table_name, metadata)
-    elif source == "twitter":
-        create_twitter_pois_table(db, pois_table_name, metadata)
-        Base.prepare(db, reflect=True)
-        TTable = getattr(Base.classes, pois_table_name)
-        # create a Session
-        session = Session(db)
-        return session, TTable
-    elif source == "gsv":
-        create_gsv_pois_table(db, pois_table_name, metadata)
         Base.prepare(db, reflect=True)
         STable = getattr(Base.classes, pois_table_name)
+        CTable = getattr(Base.classes, count_table_name)
         # create a Session
         session = Session(db)
-        return session, STable
+        return session, STable  , CTable
 
+    elif source == "fsq_matched":
+        create_fsq_pois_table(db, pois_table_name, metadata)
+
+    elif source == "google_matched":
+        create_fsq_pois_table(db, pois_table_name, metadata)
+
+    elif source == "twitter":
+        create_twitter_pois_table(db, pois_table_name, metadata)
+        # Base.prepare(db, reflect=True)
+        # TTable = getattr(Base.classes, pois_table_name)
+        # # create a Session
+        # session = Session(db)
+        # return session, TTable
+    elif source == "gsv":
+        create_gsv_pois_table(db, pois_table_name, metadata)
+        # Base.prepare(db, reflect=True)
+        # STable = getattr(Base.classes, pois_table_name)
+        # # create a Session
+        # session = Session(db)
+        # return session, STable
+    elif source == "scene_features":
+        create_scene_features_table(db, pois_table_name, metadata)
+        # Base.prepare(db, reflect=True)
+        # STable = getattr(Base.classes, pois_table_name)
+        # # create a Session
+        # session = Session(db)
+        # return session, STable
     else:
         print("ERROR, none of google, FSQ, gsv or twitter given")
         return 0
-    create_count_per_poi_table(db, count_table_name, metadata)
+    #create_count_per_poi_table(db, count_table_name, metadata)
     # reflect the tables
     Base.prepare(db, reflect=True)
-
     STable = getattr(Base.classes, pois_table_name)
-    CTable = getattr(Base.classes, count_table_name)
+    #CTable = getattr(Base.classes, count_table_name)
     #data_table = Table('google_ams_centroids_40', metadata, autoload=True)
     # create a Session
     session = Session(db)
-    return session, STable, CTable
+    return session, STable #, CTable
 
 
 def get_last_id_from_logfile(logfile):
