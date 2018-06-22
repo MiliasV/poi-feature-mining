0d941ee44d93624131fbdbe8feb76f8f53a1bdcb
bill <milias.vasilis@gmail.com>
2018-06-22 20:58:10 +0200

    different models added

--- morphological_features/object_detection_gsv.py
+++ morphological_features/object_detection_gsv.py
@@ -12,7 +12,7 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 import cv2
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 import postgis_functions
 
 # This is needed since the notebook is stored in the object_detection folder.
@@ -28,23 +28,33 @@ sys.path.append("..")
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
-# # Model preparation
 
-# ## Variables
-#
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.
-#
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+######################
+# Model preparation  #
+#####################
 
-# In[4]:
-# What model to download.
 # rfcn_resnet101_coco_2018_01_28
 #'faster_rcnn_resnet101_ava_v2.1_2018_04_30'
 #faster_rcnn_inception_resnet_v2_atrous_lowproposals_oid_2018_01_28
+# rfcn_resnet101_coco_2018_01_28
 
-MODEL_NAME = 'rfcn_resnet101_coco_2018_01_28'
+########
+# COCO #
+########
+# labels => mscoco_label_map.pbtxt
+
+# 1) Accurate
+# rcnn_nas_coco_2018_01_28 (model with highest mAP but slowest)
+
+# 2) Accurate
+# faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28
+
+# 3) Fastest
+# ssd_mobilenet_v1_coco_2017_11_17
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 REL_PATH = "/usr/local/lib/python3.6/dist-packages/tensorflow/models/research/"
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
@@ -56,7 +66,7 @@ PATH_TO_LABELS = os.path.join(REL_PATH + '/object_detection/data','mscoco_label_
 # 'mscoco_label_map.pbtxt')
 #oid_bbox_trainable_label_map.pbtxt
 
-NUM_CLASSES = 600
+NUM_CLASSES = 6000
 
 # ## Download Model
 
@@ -115,43 +125,53 @@ def load_image_into_numpy_array(image):
 # PATH_TO_TEST_IMAGES_DIR = 'test_images'
 # TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
 
+if __name__ == '__main__':
+    # Size, in inches, of the output images.
+    IMAGE_SIZE = (12, 8)
+    # indexes of categories of interest
+    # check category index
+    category_ids = [1, 2, 3, 4, 6, 7, 8, 9, 10 ,11, 13, 15, 64]
+    imgs = postgis_functions.get_photos_from_db("matched_gsv_ams", 1000)
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            for img in imgs:
+            #while True:
+                image_np = cv2.imread(img["path"])
+                #r, image_np = cap.read()
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                # Actual detection.
+                (boxes, scores, classes, num_detections) = sess.run(
+                  [boxes, scores, classes, num_detections],
+                  feed_dict={image_tensor: image_np_expanded})
 
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
+                print(scores)
+                print(classes)
+                detected_index = np.argwhere(scores>0.3)
+                print(detected_index)
+                print(np.take(classes, detected_index))
+                print(category_index)
+                #print((boxes, scores, classes, num_detections))
+                #Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                  image_np,
+                  np.squeeze(boxes),
+                  np.squeeze(classes).astype(np.int32),
+                  np.squeeze(scores),
+                  category_index,
+                  use_normalized_coordinates=True,
+                  line_thickness=3)
 
-imgs = postgis_functions.get_photos_from_db("gt_gsv_ams_matched", 0)
-# In[10]:
-
-with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
-    for img in imgs:
-      image_np = cv2.imread(img["path"])
-      #cap.read()
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-      # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
-      # Visualization of the results of a detection.
-      vis_util.visualize_boxes_and_labels_on_image_array(
-          image_np,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=8)
-
-      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-      if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+                cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
+                cv2.waitKey  (400000000)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
